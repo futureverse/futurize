814cc6255359304a62872835d1ee6dac13aa5730
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-09-26 19:06:15 -0700

    cleanup [ci skip]

--- R/find_transpiler.R
+++ R/find_transpiler.R
@@ -23,31 +23,31 @@ find_transpiler <- function(expr, envir = parent.frame(), flavor, what, debug = 
   transpilers <- transpiler_sets[[ns_name]]
   if (is.null(transpilers)) {
     if (!requireNamespace(ns_name)) {
-      stop(sprintf("Please install %s in order to futurize %s::%s()",
-           sQuote(ns_name), ns_name, fcn_name))
+      stop(sprintf("Please install %s in order to %s %s::%s()",
+           sQuote(ns_name), what, ns_name, fcn_name))
     }
     req_pkgs <- append_transpilers_for_pkg(ns_name)
     okay <- vapply(req_pkgs, FUN.VALUE = NA, FUN = requireNamespace, quietly = FALSE)
     if (!all(okay)) {
       pkgs <- req_pkgs[!okay]
-      stop(sprintf("Please install %s in order to futurize %s::%s()",
-           commaq(pkgs), ns_name, fcn_name))
+      stop(sprintf("Please install %s in order to %s %s::%s()",
+           commaq(pkgs), what, ns_name, fcn_name))
     }
     transpiler_sets <- get_transpilers(flavor)
     transpilers <- transpiler_sets[[ns_name]]
   }
 
   if (debug) {
-    mdebugf("Namespaces registered with futurize(): %s", commaq(names(transpiler_sets)))
+    mdebugf("Namespaces registered with %s(): %s", what, commaq(names(transpiler_sets)))
   }
   
   ## Is there a registered transpiler for the function?
   if (is.null(transpilers)) {
-    stop(sprintf("Function %s::%s() is not in one of the registered futurize namespaces: %s", ns_name, fcn_name, commaq(names(transpiler_sets))))
+    stop(sprintf("Function %s::%s() is not in one of the registered %s namespaces: %s", ns_name, fcn_name, what, commaq(names(transpiler_sets))))
   }
 
   if (! fcn_name %in% names(transpilers)) {
-    stop(sprintf("Do not know how to futurize function: %s()", deparse(call)))
+    stop(sprintf("Do not know how to %s function: %s()", what, deparse(call)))
   }
   transpiler <- transpilers[[fcn_name]]
   if (debug) {
