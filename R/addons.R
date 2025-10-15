78892b8d3e4f09b95b8bc8e16d43bff228d33278
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-10-14 22:54:39 -0700

    Move register_all_transpilers() to its own file

--- R/addons.R
+++ R/addons.R
@@ -47,26 +47,3 @@ make_addon_transpilers <- function(from_package, to_package, make_options) {
   names(transpilers) <- from_package
   transpilers
 } ## make_addon_transpilers()
-
-
-
-## This function registered functions that adds transpilers for specific
-## package, without loading those package.
-register_all_transpilers <- function() {
-  ## Built-in
-  transpilers_for_package("futurize::built-in", package = "base",       append_builtin_transpilers_for_base)
-  
-  ## Add-ons
-  transpilers_for_package("futurize::add-on", package = "base",         append_transpilers_for_future.apply)
-  transpilers_for_package("futurize::add-on", package = "stats",        append_transpilers_for_future.apply)
-  transpilers_for_package("futurize::add-on", package = "purrr",        append_transpilers_for_furrr)
-  transpilers_for_package("futurize::add-on", package = "crossmap",     append_transpilers_for_crossmap)
-  transpilers_for_package("futurize::add-on", package = "foreach",      append_transpilers_for_doFuture)
-  transpilers_for_package("futurize::add-on", package = "BiocParallel", append_transpilers_for_BiocParallel)
-  transpilers_for_package("futurize::add-on", package = "glmnet",       append_transpilers_for_glmnet)
-  transpilers_for_package("futurize::add-on", package = "boot",         append_transpilers_for_boot)
-  transpilers_for_package("futurize::add-on", package = "caret",        append_transpilers_for_caret)
-  transpilers_for_package("futurize::add-on", package = "lme4",         append_transpilers_for_lme4)
-  transpilers_for_package("futurize::add-on", package = "plyr",         append_transpilers_for_plyr)
-  transpilers_for_package("futurize::add-on", package = "tm",           append_transpilers_for_tm)
-} ## register_all_transpilers()
