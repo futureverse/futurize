524195764139d4f845ed2aefc801f901da2db45a
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-01-07 14:21:44 -0800

    Reuse make_options_for_doFuture() for BiocParallel and make_options_for_furrr() for crossmap

--- R/addons-crossmap.R
+++ R/addons-crossmap.R
@@ -4,13 +4,7 @@
 #
 append_transpilers_for_crossmap <- function() {
   append_transpilers("futurize::add-on", make_addon_transpilers(
-    "crossmap", "crossmap", make_options = function(options, fcn) {
-      keep <- intersect(names(formals(furrr::furrr_options)), names(options))
-      options <- options[keep]
-      options <- do.call(furrr::furrr_options, args = options)
-      options <- list(.options = options)
-      options
-    })
+    "crossmap", "crossmap", make_options = make_options_for_furrr)
   )
 
   ## Return required packages
