98750ffc08b0afcdc0b5e254321d37db87b1ec25
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-01-16 17:23:11 -0800

    TESTS: increase test coverage

--- inst/testme/test-utils.R
+++ inst/testme/test-utils.R
@@ -1,7 +1,7 @@
 pkgs <- futurize::futurize_supported_packages()
 print(pkgs)
 
-for (pkg in pkgs) {
+for (pkg in c(pkgs, "future", "aNonExistingPackage")) {
   if (requireNamespace(pkg)) {
     cat(sprintf("Package %s:\n", pkg))
     fcns <- tryCatch({
