e9d5c31abc416a6f0346bdf171a9b03b6f11585a
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-03-31 15:56:26 -0700

    Add default future labels for 'plyr' [#10]

--- R/addons-plyr.R
+++ R/addons-plyr.R
@@ -12,17 +12,19 @@ append_transpilers_for_plyr <- function() {
     with(doFuture::registerDoFuture(flavor = "%dofuture%"), (.(EXPR)))
   )
 
-  transpiler <- function(expr, options = NULL) {
-    options <- make_options_for_doFuture(options, wrap = TRUE)
+  template2 <- bquote_compile(function(expr, options = NULL) {
+    defaults <- list(label = sprintf("fz:plyr::%s-%%d", .(NAME)))
+    options <- make_options_for_doFuture(options, defaults = defaults, wrap = TRUE)
     expr <- append_call_arguments(expr,
       .parallel = TRUE,
       .paropts = options
     )
     bquote_apply(template, EXPR = expr)
-  }
+  })
 
   transpilers <- make_package_transpilers("plyr", FUN = function(fcn, name) {
     if (".parallel" %in% names(formals(fcn))) {
+      transpiler <- eval(bquote_apply(template2, NAME = name))
       list(
         label = sprintf("plyr::%s() ~> plyr::%s(..., parallel = TRUE)", name, name),
         transpiler = transpiler
